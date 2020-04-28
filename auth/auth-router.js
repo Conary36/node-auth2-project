f997ce5570264cba722b4b5f68e155f2c0c00a12
Conary36 <cbeckford144@gmail.com>
2020-04-28 19:33:10 -0400

    token returned within /login res.status()

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -29,8 +29,10 @@ router.post("/login", (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         req.session.user = username;
         const token = generateToken(user);
-        res.status(200).json({ message: "Welcome!" });
-        return token;
+        res.status(200).json({ message: `Welcome ${user.username}!`, 
+        token
+      });
+
       } else {
         res.status(401).json({ message: "Info not matching" });
       }
